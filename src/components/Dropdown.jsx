5c6c7ab64f6238377746d8247adece2350173077
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-08-18 19:26:27 +0530

    fix: dropdown from select menu drifting out of the screen; style: made the borders more rounded

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 function Dropdown() {
     return (
-        <div className="absolute top-12 left-0 min-w-48 rounded-lg">
+        <div className="absolute top-12 right-0 min-w-48 z-10 rounded-lg shadow-lg">
             <DropdownItem />
             <DropdownItem />
             <DropdownItem />
@@ -11,7 +11,7 @@ function Dropdown() {
 
 function DropdownItem() {
     return (
-        <div className="flex p-3 gap-x-4 items-center bg-surface-bright text-on-surface border-b border-b-outline first:rounded-t-lg last:border-none last:rounded-b-lg cursor-pointer">
+        <div className="flex p-3 gap-x-4 items-center bg-surface-bright text-on-surface border-b border-b-outline first:rounded-t-xl last:border-none last:rounded-b-xl cursor-pointer">
             <div className="h-6 w-6">
                 <img className="h-full w-full object-center object-cover" src="/public/icons/commit-filled.png" />
             </div>
