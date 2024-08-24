db2474960d3bb93bd3f9c23abd06e4fe850a1082
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-08-25 01:11:36 +0530

    refactor: moved out file nav component into a different file

--- src/pages/Tree/FileNav.jsx
+++ src/pages/Tree/FileNav.jsx
@@ -1,6 +1,36 @@
 function FileNav() {
-    return <div>This is File Nav. </div>
+    return (
+        <div className="absolute py-6 px-4 bottom-0 left-0 w-full h-2/3 bg-surface-container z-10 rounded-t-3xl shadow-md">
+            <FileNavItem />
+            <FileNavItem />
+            <FileNavItem />
+        </div>
+    );
 }
+
+
+function FileNavItem() {
+    return (
+        <section className="flex my-1 gap-x-4 py-3 px-2 hover:bg-outline rounded-xl">
+            <div className="flex gap-x-1 items-center">
+                <div className="h-5 w-5">
+                    <img
+                        className="h-full w-full object-center object-cover"
+                        src="/icons/arrow-right.png"
+                    />
+                </div>
+                <div className="h-6 w-6">
+                    <img
+                        className="h-full w-full object-center object-cover"
+                        src="/icons/dir-filled.png"
+                    />
+                </div>
+            </div>
+            <p>public</p>
+        </section>
+    );
+}
+
 
 
 export default FileNav;
--- src/pages/Tree/index.jsx
+++ src/pages/Tree/index.jsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/Layout";
 import { FileExplorerHeader } from "../../components/FileExplorer";
 import SelectMenu from "../../components/SelectMenu";
+import FileNav from "./FileNav";
 
 
 function Tree() {
@@ -33,40 +34,6 @@ function CodeLine() {
             <p className="px-2 bg-surface-container">1</p>
             <p className="pl-2 bg-[#000] w-full">package arrays</p>
         </div>
-    );
-}
-
-
-function FileNav() {
-    return (
-        <div className="absolute py-6 px-4 bottom-0 left-0 w-full h-2/3 bg-surface-container z-10 rounded-t-3xl shadow-md">
-            <FileNavItem />
-            <FileNavItem />
-            <FileNavItem />
-        </div>
-    );
-}
-
-
-function FileNavItem() {
-    return (
-        <section className="flex my-1 gap-x-4 py-3 px-2 hover:bg-outline rounded-xl">
-            <div className="flex gap-x-1 items-center">
-                <div className="h-5 w-5">
-                    <img
-                        className="h-full w-full object-center object-cover"
-                        src="/icons/arrow-right.png"
-                    />
-                </div>
-                <div className="h-6 w-6">
-                    <img
-                        className="h-full w-full object-center object-cover"
-                        src="/icons/dir-filled.png"
-                    />
-                </div>
-            </div>
-            <p>public</p>
-        </section>
     );
 }
 
