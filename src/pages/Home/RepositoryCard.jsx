961bc9c59c385e952207ae55873d59449bc867e6
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-09-01 19:41:49 +0530

    style: adjusted spacing and text in repository card component

--- src/pages/Home/RepositoryCard.jsx
+++ src/pages/Home/RepositoryCard.jsx
@@ -1,32 +1,45 @@
 function RepositoryCard({ repo }) {
     return (
-        <div
-            className="mt-6 pb-4 w-full md:max-w-lg text-on-backround rounded-3xl bg-surface-container shadow-lg border-outline"
-        >
-            <div className="p-4 bg-tertiary text-sm text-on-tertiary rounded-t-3xl">
+        <div className="mt-6 w-full md:max-w-lg text-on-backround rounded-3xl bg-surface-container shadow-lg border-outline">
+            <div className="p-4 bg-tertiary text-on-tertiary rounded-t-3xl">
                 <div className="flex gap-x-2">
                     <div className="h-8 w-8">
-                        <img className="h-full w-full object-center object-cover" src="/public/icons/repo-icon-fill.png" />
+                        <img
+                            className="h-full w-full object-center object-cover"
+                            src="/public/icons/repo-icon-fill.png"
+                        />
                     </div>
                     <div>
-                        <p className="text-xl font-bold">{ repo.name }</p>
-                        <p>By { repo.author }</p>
+                        <p className="text-2xl font-bold">{ repo.name }</p>
+                        <p className="text-sm">By { repo.author }</p>
                     </div>
                 </div>
-                <div className="mt-2 py-1 flex gap-x-2">
+                <div className="mt-2 py-1 flex gap-x-2 items-center">
                     <div className="h-4 w-4">
-                        <img className="h-full w-full object-center object-cover" src="/public/icons/clock-outline.png" />
+                        <img
+                            className="h-full w-full object-center object-cover"
+                            src="/public/icons/clock-outline.png"
+                        />
                     </div>
-                    <p>Created at: { repo.created_at }</p>
+                    <div className="w-full flex justify-between">
+                        <p>Created at: </p>
+                        <p>{ repo.created_at }</p>
+                    </div>
                 </div>
-                <div className="flex gap-x-2 items-center">
+                <div className="w-full flex gap-x-2 items-center">
                     <div className="h-4 w-4">
-                        <img className="h-full w-full object-center object-cover" src="/public/icons/clock-outline.png" />
+                        <img
+                            className="h-full w-full object-center object-cover"
+                            src="/public/icons/clock-outline.png"
+                        />
                     </div>
-                    <p>Last Commit at: { repo.last_commit_at }</p>
+                    <div className="w-full flex justify-between">
+                        <p>Last Commit at: </p>
+                        <p>{ repo.last_commit_at }</p>
+                    </div>
                 </div>
             </div>
-            <p className="mt-4 px-4 text-on-surface-nv">{ repo.desc }</p>
+            <p className="px-4 py-2 text-sm text-on-surface-nv">{ repo.desc }</p>
         </div>
     );
 }
