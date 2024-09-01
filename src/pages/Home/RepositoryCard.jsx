6175ef241683217912f3c2c00e70b4552b4f0e3d
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-09-01 17:48:26 +0530

    feat: implemented api to list all the repositories in the server

--- src/pages/Home/RepositoryCard.jsx
+++ src/pages/Home/RepositoryCard.jsx
@@ -1,45 +1,35 @@
-function Repositories() {
+function RepositoryCard({ repo }) {
     return (
-        <div>
-            <h1 className="text-3xl font-bold">Repositories</h1>
-
-            <RepositoryCard />
-            <RepositoryCard />
-            <RepositoryCard />
-        </div>
-    );
-}
-
-function RepositoryCard() {
-    return (
-        <div className="mt-6 pb-4 w-full text-on-backround rounded-3xl bg-surface-container shadow-lg border-outline">
+        <div
+            className="mt-6 pb-4 w-full text-on-backround rounded-3xl bg-surface-container shadow-lg border-outline"
+        >
             <div className="p-4 bg-tertiary text-sm text-on-tertiary rounded-t-3xl">
                 <div className="flex gap-x-2">
                     <div className="h-8 w-8">
                         <img className="h-full w-full object-center object-cover" src="/public/icons/repo-icon-fill.png" />
                     </div>
                     <div>
-                        <p className="text-xl font-bold">Kata Dojo</p>
-                        <p>By Arihant</p>
+                        <p className="text-xl font-bold">{ repo.name }</p>
+                        <p>By { repo.author }</p>
                     </div>
                 </div>
                 <div className="mt-2 py-1 flex gap-x-2">
                     <div className="h-4 w-4">
                         <img className="h-full w-full object-center object-cover" src="/public/icons/clock-outline.png" />
                     </div>
-                    <p>Created at: Jun 21, 2022 01:28 PM</p>
+                    <p>Created at: { repo.created_at }</p>
                 </div>
                 <div className="flex gap-x-2 items-center">
                     <div className="h-4 w-4">
                         <img className="h-full w-full object-center object-cover" src="/public/icons/clock-outline.png" />
                     </div>
-                    <p>Last Commit at: Jun 21, 2022 01:28 PM</p>
+                    <p>Last Commit at: { repo.last_commit_at }</p>
                 </div>
             </div>
-            <p className="mt-4 px-4 text-on-surface-nv">This is a very long description.</p>
+            <p className="mt-4 px-4 text-on-surface-nv">{ repo.desc }</p>
         </div>
     );
 }
 
 
-export default Repositories;
+export default RepositoryCard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
-import Repositories from "./Repositories";
+import RepositoryCard from "./RepositoryCard";
+
 
 function Home() {
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    const fetchRepositories = async () => {
+        try {
+            const response = await fetch("http://localhost:8000/api/v1/repos");
+            if (!response.ok) {
+                throw new Error("Failed to fetch the repositories");
+            }
+            const data = await response.json();
+            setRepos(data.data);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchRepositories();
+    }, [])
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     return (
         <Layout>
-            <Repositories />
+            <h1 className="text-3xl font-bold">Repositories</h1>
+            {repos.map((repo, idx) => (
+                <RepositoryCard key={idx} repo={repo} />
+            ))}
         </Layout>
     );
 }
