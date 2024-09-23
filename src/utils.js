0f19a37fe0d3a54ac1eec43bb06f8fee8b40b49a
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-09-24 01:42:20 +0530

    refactor: deleted utils.jsx and moved its function in util.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,16 @@
+const MAX_COMMIT_CARD_BOXES = 6;
+
+
+export const calculatedDeletionBoxes = (insertions, deletions) => {
+    const iInsertions = parseInt(insertions);
+    const iDeletions = parseInt(deletions);
+
+    const deletionPercentage = iDeletions / (iInsertions + iDeletions);
+
+    return Math.round(deletionPercentage * MAX_COMMIT_CARD_BOXES);
+}
+
+
 export const getDirPath = (path) => {
     const pathSplits = path.split("/");
     if (pathSplits.length <= 5) {
