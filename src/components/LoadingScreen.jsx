b62e4ed86a833d4e1fc435ede7b0fad9d4f18396
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-11-23 14:58:59 +0530

    feat: added loading screen animation

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,7 +1,7 @@
 function LoadingScreen() {
     return (
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-            <div className="h-6 w-6">
+            <div className="h-6 w-6 loader">
                 <img
                     src="/icons/loading.png"
                     className="h-full w-full object-center object-cover"
