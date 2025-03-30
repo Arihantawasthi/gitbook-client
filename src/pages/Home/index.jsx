2a197e829f91484a7f7414f94a378c81b3022572
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2025-03-31 02:27:34 +0530

    feat: added link for redirecting to repos page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Layout from "../../components/Layout";
 
 import RepoCardContainer from "./RepoCardContainer";
@@ -6,7 +7,20 @@ import Stats from "./Stats";
 function Home() {
     return (
         <Layout>
-            <h1 className="text-3xl font-bold">Repositories</h1>
+            <div className="w-full flex justify-between items-center">
+                <h1 className="text-3xl font-bold">Repositories</h1>
+                <Link
+                    to={"repos"}
+                    className="flex gap-x-2 items-center cursor-pointer"
+                >
+                    <p className="text-lg">View All</p>
+                    <img
+                        className="h-6 w-6"
+                        src="/icons/arrow-right.png"
+                        alt="view all repos"
+                    />
+                </Link>
+            </div>
             <RepoCardContainer size={4} />
             <h1 className="mt-12 text-3xl font-bold">Stats</h1>
             <Stats />
