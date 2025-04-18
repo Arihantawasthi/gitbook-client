de6c8ecd41df29ea0d81733f423b989db0c12477
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2025-04-18 17:27:15 +0530

    fix: back icon appearing on every diff section

--- src/pages/Diff/DiffFileBlock.jsx
+++ src/pages/Diff/DiffFileBlock.jsx
@@ -1,34 +1,13 @@
-import { useNavigate } from "react-router-dom"
-
 function DiffFileBlock({ object }) {
-    const navigate = useNavigate();
-
-    const goBack = () => {
-        if (window.history.length > 1) {
-            navigate(-1);
-        }
-    }
-
     return (
-        <>
-            <div
-                className="h-8 w-8 cursor-pointer"
-                onClick={goBack}
-            >
-                <img
-                    className="h-full w-full object-center object-cover"
-                    src="/icons/arrow-back.png"
-                />
+        <div className="bg-code rounded-2xl">
+            <div className="p-4 rounded-t-2xl bg-surface-container">
+                <div className="underline">{ object.file_path }</div>
             </div>
-            <div className="bg-code rounded-2xl">
-                <div className="p-4 rounded-t-2xl bg-surface-container">
-                    <div className="underline">{ object.file_path }</div>
-                </div>
-                <div className="p-4">
-                    { object.code_lines.map((line, idx) => <DiffLine key={idx} line={ line } />) }
-                </div>
+            <div className="p-4">
+                { object.code_lines.map((line, idx) => <DiffLine key={idx} line={ line } />) }
             </div>
-        </>
+        </div>
     )
 }
 
--- src/pages/Diff/index.jsx
+++ src/pages/Diff/index.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Layout from "../../components/Layout";
 import RepoDesc from "../../components/RepoDesc";
 import DiffFileBlock from "./DiffFileBlock";
@@ -6,8 +6,8 @@ import LoadingScreen from "../../components/LoadingScreen";
 
 import useFetchDiff from "../../hooks/useFetchDiff";
 
-
 function Diff() {
+    const navigate = useNavigate();
     const { repoName } = useParams("repoName");
     const { commitHash } = useParams("hash");
     const { data, loading, error } = useFetchDiff(repoName, commitHash);
@@ -19,10 +19,26 @@ function Diff() {
         return <div>Oops! An Error occured</div>
     }
 
+
+    const goBack = () => {
+        if (window.history.length > 1) {
+            navigate(-1);
+        }
+    }
+
     return (
         <Layout>
             <RepoDesc repoName={repoName} repoDesc={commitHash} />
-            <div className="mt-6 flex flex-col gap-y-4">
+            <div
+                className="mt-6 h-8 w-8 cursor-pointer"
+                onClick={goBack}
+            >
+                <img
+                    className="h-full w-full object-center object-cover"
+                    src="/icons/arrow-back.png"
+                />
+            </div>
+            <div className="mt-2 flex flex-col gap-y-4">
                 { data.map((object, idx) => <DiffFileBlock key={idx} object={object} />) }
             </div>
         </Layout>
